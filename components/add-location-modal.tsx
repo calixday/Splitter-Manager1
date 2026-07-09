77c22457781011e01413099965b7aa825587398f
KOECH AMOS <142664159+calixday@users.noreply.github.com>
2026-07-10 00:13:32 +0300

    Update add-location-modal.tsx

--- components/add-location-modal.tsx
+++ components/add-location-modal.tsx
@@ -9,7 +9,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
-const PREDEFINED_MODELS = ["JT C650", "ADHS C650 1", "ADHS C650 2","WOODMERE C320", "NRB MILIMANI C320", "KAREN C650", "RUBIA C320"]
+const PREDEFINED_MODELS = ["ADHS C650 1", "ADHS C650 2","WOODMERE C320","JT C650", "NRB MILIMANI C320", "KAREN C650", "RUBIA C320"]
 
 interface AddLocationModalProps {
   open: boolean
